0003199650d78b293f1f80d1a6b0800b72320418
user <user@user-PC>
2017-02-02 10:46:15 +0900

    Create Java Project edit by Kevin29Shim 자바 프로젝트 생성 수정 - 심현보

---
+++ Hello_Git/src/Test/Hello_Java.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Hello_Java {
+
+	public static void main(String[] args) {
+		System.out.println("H E L L O _ G I T");
+		System.out.println("First Commit Work");
+	}
+	
+
+}
