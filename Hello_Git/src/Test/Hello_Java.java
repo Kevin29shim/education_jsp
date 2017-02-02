a5560730dc046cc7fc998f44e9a98053c2b9e80c
user <user@user-PC>
2017-02-02 11:07:10 +0900

    Make Branch Work - Hyunbo

--- Hello_Git/src/Test/Hello_Java.java
+++ Hello_Git/src/Test/Hello_Java.java
@@ -5,6 +5,9 @@ public class Hello_Java {
 	public static void main(String[] args) {
 		System.out.println("H E L L O _ G I T");
 		System.out.println("First Commit Work");
+		
+		//branch
+		System.out.println("Create Branch!! - ShimHyunbo");
 	}
 	
 
